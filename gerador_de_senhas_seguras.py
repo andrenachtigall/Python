4ecbfa973162f1ad2be5e8f6c83f7a8de95cf003
andrenachtigall <43154805+andrenachtigall@users.noreply.github.com>
2023-03-25 02:04:23 -0300

    Update gerador_de_senhas_seguras.py

--- gerador_de_senhas_seguras.py
+++ gerador_de_senhas_seguras.py
@@ -2,7 +2,7 @@ import random
 
 letra_maiuscula = chr(random.randint(65,90))
 letra_minuscula = chr(random.randint(97,122))
-char_especial = chr(random.randint(33,47)
+char_especial = chr(random.randint(33,47))
 lista_numeros = []
 
 for i in range(5): # Até 8 digitos
@@ -11,5 +11,6 @@ for i in range(5): # Até 8 digitos
 random.shuffle(lista_numeros)
 lista_numeros = str(lista_numeros) [1:-1]
 lista_numeros = lista_numeros.replace(',', '')
-
-print(letra_maiuscula, letra_minuscula, lista_numeros, char_especial)
+senha = letra_maiuscula+letra_minuscula+lista_numeros+char_especial
+senha = senha.replace(' ','')
+print(senha)
